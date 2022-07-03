b32edeaf6ed2b55b018d25cab3048300c6afc308
gdahl-ivy <gdahl1@ivytech.edu>
2022-07-03 15:37:47 -0500

    added KeyboardInterrupt functionality

--- terminal_mockup/ui.py
+++ terminal_mockup/ui.py
@@ -2,18 +2,26 @@
 Terminal UI functionality for the terminal mockup
 """
 
+from sys import exit
+
+
 def start_display() -> None:
     """Displays the initial state of the application"""
     print('--------------------')
     print('| Student Enroller |')
     print('--------------------')
-    print('Log In')
+    print('Log In (^C to exit)')
 
 
 def prompt_login() -> tuple[str, str]:
     """Prompts the user to enter their username and password and returns them"""
-    username = input('username: ')
-    password = input('password: ')
+    try:
+        username = input('username: ')
+        password = input('password: ')
+    except KeyboardInterrupt:
+        print('\nexiting...')
+        exit()
+    
     print()
     return username, password
 
