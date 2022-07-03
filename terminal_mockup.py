9b28b316c5502bb9105230c3318f56ac56ae4771
gdahl-ivy <gdahl1@ivytech.edu>
2022-07-03 01:21:38 -0500

    started making terminal_mockup

---
+++ terminal_mockup.py
@@ -0,0 +1,15 @@
+"""
+Terminal Proof-of-Concept
+
+This program is meant to contain the functionality of the GUI app we are building 
+without worrying about the graphical part. It will allow us figure out the backend 
+implementation before (or as) we make the front end.
+"""
+
+
+def main():
+    print('hello')
+
+
+if __name__ == '__main__':
+    main()
