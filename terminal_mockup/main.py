2e96e53d5fb0f591f63c83924c38289217968daa
gdahl-ivy <gdahl1@ivytech.edu>
2022-07-07 14:46:26 -0500

    further separated io, resources, and logic

--- terminal_mockup/main.py
+++ terminal_mockup/main.py
@@ -8,14 +8,36 @@ implementation before (or as) we make the front end.
 This is the entry point for the program.
 """
 
-import enroller.ui as ui
-import enroller.logic as logic
+import enroller.terminal_io as io
+import enroller.resources as resources
 
 
 def main() -> None:
-    ui.start_display()
-    username = logic.login()
-    logic.select_courses(username)
+    """Runs the terminal app"""
+
+    # login
+    io.start_display()
+    users = resources.get_users()
+    username, password = io.prompt_login()
+
+    while not authenticate(users, username, password):
+        io.display_invalid_login()
+        username, password = io.prompt_login()
+
+    # course select
+    courses = resources.get_courses()
+    while True:
+        io.display_courses(courses)
+        selected = io.select_course(courses)
+        io.display_new_enrollment(selected)
+
+
+def authenticate(users: 'dict[str, str]', username: str, password: str) -> bool:
+    """Determines whether the username-password pair appear in the users dictionary together"""
+    try:
+        return users[username] == password
+    except KeyError:
+        return False
 
 
 if __name__ == '__main__':
